888264d34f62f1e366847a85e4bb2646a5cd9e2a
MathieuWEILAN <91557847+MathieuWEILAN@users.noreply.github.com>
2025-10-13 15:20:30 +0200

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="">
+      <div className="h-screen bg-red-500"></div>
+      <div className="h-screen bg-blue-500"></div>
+      <div className="h-screen bg-green-500"></div>
+    </div>
+  );
+}
